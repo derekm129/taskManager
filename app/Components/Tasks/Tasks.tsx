c122ee8010d770a7c3e2b1d9c2a39d926b03006d
dmod129 <derek.modugno@gmail.com>
2025-09-23 20:48:13 -0500

    fixed errors in route.ts

--- app/Components/Tasks/Tasks.tsx
+++ app/Components/Tasks/Tasks.tsx
@@ -3,11 +3,16 @@ import { useGlobalState } from "@/app/context/globalProvider";
 import React from "react";
 import styled from "styled-components";
 import CreateContent from "../Models/CreateContent";
+import TaskItem from "../TaskItem/TaskItem";
+import { plus } from "@/app/utils/Icons"; 
+
 
 interface Task {
-  id: number;
-  name: string;
-  completed: boolean;
+    title: string;
+    description: string;
+    date: string;
+    isCompleted: boolean;
+    id: string;
 }
 
 interface Props {
@@ -18,14 +23,27 @@ interface Props {
 function Tasks({title, tasks}: Props) {
     const { theme } = useGlobalState();
 
-    return <TaskStyled theme={theme}>
-        <h2>{title}</h2>
+    return (
+    <TaskStyled theme={theme}>
+        <h1>{title}</h1>
         <div className="tasks grid">
             {tasks.map((task) => (
-                <TaskItem key={task.id} task={{ ...task }} />
+                <TaskItem 
+                key={task.id} 
+                title={task.title}
+                description={task.description}
+                date={task.date}
+                isCompleted={task.isCompleted}
+                id={task.id}
+                />
             ))}
+            <div className="create-task">
+                { plus }
+                Add New Task
+            </div>
         </div>
-    </TaskStyled>;
+    </TaskStyled>
+    );
 }
 
 
@@ -42,6 +60,48 @@ const TaskStyled = styled.main`
     &::-webkit-scrollbar {
         width: 0.5rem;
      }
+    
+    > h1 {
+    font-size: clamp(1.5rem, 2vw, 2rem);
+    font-weight: 800;
+    position: relative;
+
+     &::after {
+      content: "";
+      position: absolute;
+      bottom: -0.5rem;
+      left: 0;
+      width: 3rem;
+      height: 0.2rem;
+      background-color: ${(props) => props.theme.colorPrimaryGreen};
+      border-radius: 0.5rem;
+    }
+}
+
+  .create-task {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+
+    height: 16rem;
+    color: ${(props) => props.theme.colorGrey2};
+    font-weight: 600;
+    cursor: pointer;
+    border-radius: 1rem;
+    border: 3px dashed ${(props) => props.theme.colorGrey5};
+    transition: all 0.3s ease;
+
+    i {
+      font-size: 1.5rem;
+      margin-right: 0.2rem;
+    }
+
+    &:hover {
+      background-color: ${(props) => props.theme.colorGrey5};
+      color: ${(props) => props.theme.colorGrey0};
+    }
+  }
 `
 
 export default Tasks;
--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,86 +1,96 @@
 import prisma from "@/app/utils/connect";
-import { getAuth, auth } from "@clerk/nextjs/server";
-import { NextResponse, NextRequest } from "next/server";
-
-export async function POST(req: NextRequest) {
-    try {
-        const { userId } = getAuth(req);
-    if(!userId) {
-        return NextResponse.json({ error: "Unauthorized", status: 401 });
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+// POST
+export async function POST(req: Request) {
+  try {
+    
+    const authData = await auth();
+    if (!authData?.userId) {
+      return NextResponse.json({ error: "Unauthorized", status: 401 });
     }
+    const userId: string = authData.userId!; 
 
-    const {title, description, date, completed, important} = await req.json();
+    const { title, description, date, completed, important } = await req.json();
 
     if (!title || !description || !date) {
-        return NextResponse.json({
-            error: "Missing required fields",
-            status: 400,
-
-        });
+      return NextResponse.json({
+        error: "Missing required fields",
+        status: 400,
+      });
     }
-    
+
     if (title.length < 3) {
-        return NextResponse.json({
-            error: "Title must be at least 3 characters long",
-            status: 400,
-        });
+      return NextResponse.json({
+        error: "Title must be at least 3 characters long",
+        status: 400,
+      });
     }
 
     const task = await prisma.task.create({
-        data: {
-            title,
-            description,
-            date,
-            isCompleted: completed,
-            isImportant: important,
-            userId
-        },
+      data: {
+        title,
+        description,
+        date,
+        isCompleted: completed,
+        isImportant: important,
+        userId,
+      },
     });
 
-    console.log("TASK CREATED: ", task);
-    
     return NextResponse.json(task);
-    } catch (error) {
-        console.log("ERROR CREATING TASK: ", error);
-        return NextResponse.json({ error: "Error creating task", status: 500 });
-    }
+  } catch (error) {
+    console.log("ERROR CREATING TASK: ", error);
+    return NextResponse.json({ error: "Error creating task", status: 500 });
+  }
 }
+// GET
+export async function GET(req: Request) {
+  try {
+    const authData =  await auth();
 
-export async function GET(req: NextRequest) {
-    try {
-        const { userId } = getAuth(req);
-
-        if(!userId) {
-            return NextResponse.json({ error: "Unauthorized", status: 401 });
-        }
-
-        const tasks = await prisma.task.findMany({
-            where: {
-                userId,
-            }
-        });
-
-        console.log("TASKS: ", tasks);
-        return NextResponse.json(tasks);
-        
-    } catch (error) {
-        console.log("ERROR GETTING TASK: ", error);
-        return NextResponse.json({ error: "Error getting task", status: 500 });
+    if (!authData?.userId) {
+      return NextResponse.json({ error: "Unauthorized", status: 401 });
     }
+
+    const userId: string = authData.userId!; 
+    const tasks = await prisma.task.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    return NextResponse.json(tasks);
+  } catch (error) {
+    console.log("ERROR GETTING TASKS: ", error);
+    return NextResponse.json({ error: "Error updating task", status: 500 });
+  }
 }
+// PUT
+export async function PUT(req: Request) {
+  try {
+    const authData =  await auth();
 
-export async function PUT(req: NextRequest) {
-    try {
-    } catch (error) {
-        console.log("ERROR UPDATING TASK: ", error);
-        return NextResponse.json({ error: "Error updating task", status: 500 });
+    if (!authData?.userId) {
+      return NextResponse.json({ error: "Unauthorized", status: 401 });
     }
-}
 
-export async function DELETE(req: NextRequest) {
-    try {
-    } catch (error) {
-        console.log("ERROR DELETING TASK: ", error);
-        return NextResponse.json({ error: "Error deleting task", status: 500 });
+    const userId: string = authData.userId!; // TypeScript-safe
+
+    const { isCompleted, id } = await req.json();
+
+    if (!id) {
+      return NextResponse.json({ error: "Task ID is required", status: 400 });
     }
+
+    const task = await prisma.task.update({
+      where: { id },
+      data: { isCompleted },
+    });
+
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log("ERROR UPDATING TASK:", error);
+    return NextResponse.json({ error: "Error updating task", status: 500 });
+  }
 }
